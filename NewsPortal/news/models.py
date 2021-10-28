3847cbd470551ebb9655469faac46e147a73c27c
Aleksey <Kaloha215@gmail.com>
2021-10-28 11:48:12 +1000

    finish

--- NewsPortal/news/models.py
+++ NewsPortal/news/models.py
@@ -53,7 +53,6 @@ class Post(models.Model):
         return f'{self.text[:125]} + {"..."}'
 
 
-
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
